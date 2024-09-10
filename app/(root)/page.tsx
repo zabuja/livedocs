ec42fe721d0ca642be1656599597b51a725e25a8
atanu <atanu.tmsl@gmail.com>
2024-09-10 12:36:01 +0530

    feat: homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,32 @@
-import Header from '@/components/Header'
-import { Button } from '@/components/ui/button'
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import React from 'react'
+import AddDocumentButton from "@/components/AddDocumentButton";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import React from "react";
 
 const Home = () => {
+  const documents = [];
+
   return (
-    <main className='home-container'>
-      <Header className='sticky left-0 top-0'>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
           Notifications
           <SignedIn>
-            <UserButton/>
+            <UserButton />
           </SignedIn>
         </div>
       </Header>
+      {documents.length > 0 ? (
+        <div>zx</div>
+      ) : (
+        <div className="document-list-empty">
+          <Image src="/assets/icons/doc.svg" alt="Document" height={40} width={40} className="mx-auto"/>
+          <AddDocumentButton/>
+        </div>
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ components/AddDocumentButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddDocumentButton = () => {
+  return (
+    <div>AddDocumentButton</div>
+  )
+}
+
+export default AddDocumentButton
